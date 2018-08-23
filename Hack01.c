14d3f817700a60a7214537460652cfbbef634df4
SergioCarrera <carrera.sergio98@yahoo.com>
2018-08-23 10:03:10 -0500

    Initial Commit

---
+++ Hack01.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sergio Carrera
+ * Date: 2018/23/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sergio Carrera, Computer Science\n");
+
+  return 0;
+}
